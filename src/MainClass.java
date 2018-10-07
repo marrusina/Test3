4ce6e9f1c790662b257bec8a8104e5bbe166cce7
marina rusina <rusina@reksoft.ru>
2018-10-07 09:58:21 +0300

    a.contains
    a.contains||a.contains

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,20 +1,11 @@
 public class MainClass
 {
-    private String  class_string = "Hello, world";
-    private String class_hello = "hello";
-    private String class_Hello = "Hello";
+    private String  class_string = " Hello, world";
+
 
     public String getClassString()
     {
         return class_string;
-    }
-    public String getClassStringHello()
-    {
-        return class_Hello;
-    }
-    public String getClassStringHhello()
-    {
-        return class_hello;
     }
 
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -8,16 +8,14 @@ public class MainClassTest extends MainClass
     public void testGetClassString()
     {
         String a = getClassString();
-        String b = getClassStringHello();
-        String c = getClassStringHhello();
-
-        if (a.isEmpty())
+            if (a.isEmpty())
         {
             Assert.fail("The string is empty");
-        } else {
-            System.out.println(a.regionMatches(true, 0, b, 0, 5));
-            System.out.println(a.regionMatches(true, 0, c, 0, 5));
+        } else if (a.contains("Hello")||a.contains("hello"))
+        {
+            System.out.println("Good sentence");
         }
-
     }
+
 }
+
